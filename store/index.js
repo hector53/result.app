177231166013a3c930a2ea15e535e57c08be9614
hector53 <hectoracosta5@gmail.com>
2021-07-22 11:12:50 -0400

    oiajsdos

--- store/index.js
+++ store/index.js
@@ -398,7 +398,6 @@ export const actions = {
          if (process.server && process.static) return;
 
 
-          const req = req
           const headers = (req && req.headers) ? Object.assign({}, req.headers) : {}
           const xForwardedFor = headers['x-forwarded-for']      
           const xRealIp = headers['x-real-ip']
